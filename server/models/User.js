a80a73532a937f33cee52ad79b58c51ebd1ce9ac
Mark <mark3fullen@gmail.com>
2023-03-05 16:30:47 -0500

    Deleting node_modules

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true},
-    password: { type: String, required: true}
-})
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
 
-module.exports = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
